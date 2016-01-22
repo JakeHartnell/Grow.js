8f13a41a3ce5c57754a190ec4a855955e12c3149
RawKStar77 <RawKStar77@aol.com>
2016-01-22 13:00:09 -0800

    Initial changes, trying to flesh out api.

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ var DDPClient = require('ddp');
 EJSON = require("ddp-ejson");
 var util = require('util');
 var Duplex = require('stream').Duplex;
+var Readable = require('stream').Readable;
+var Writable = require('stream').Writable;
+var fs = require('fs');
+var cron = require('cron')
 
-function CommonGarden(options) {
+function GROWJS(options) {
   var self = this;
 
-  if (!(self instanceof CommonGarden)) {
-    return new CommonGarden(options);
+  if (!(self instanceof GROWJS)) {
+    return new GROWJS(options);
   }
 
   self.options = _.clone(options || {});
@@ -34,19 +38,19 @@ function CommonGarden(options) {
   }));
 }
 
-util.inherits(CommonGarden, Duplex);
+util.inherits(GROWJS, Duplex);
 
-CommonGarden.prototype.connect = function (callback) {
+GROWJS.prototype.connect = function (callback) {
   var self = this;
 
   self.ddpclient.connect(function (error, wasReconnect) {
     if (error) return callback(error);
 
     if (wasReconnect) {
-      console.log("Reestablishment of a CommonGarden connection.");
+      console.log("Reestablishment of a GROWJS connection.");
     }
     else {
-      console.log("CommonGarden connection established.");
+      console.log("GROWJS connection established.");
     }
 
     if (self.uuid || self.token) {
@@ -74,7 +78,7 @@ CommonGarden.prototype.connect = function (callback) {
   });
 };
 
-CommonGarden.prototype._afterConnect = function (callback, result) {
+GROWJS.prototype._afterConnect = function (callback, result) {
   var self = this;
 
   self.ddpclient.subscribe(
@@ -102,7 +106,7 @@ CommonGarden.prototype._afterConnect = function (callback, result) {
   );
 };
 
-CommonGarden.prototype.sendData = function (data, callback) {
+GROWJS.prototype.sendData = function (data, callback) {
   var self = this;
 
   if (!self.ddpclient || !self.uuid || !self.token) {
@@ -121,7 +125,7 @@ CommonGarden.prototype.sendData = function (data, callback) {
   );
 };
 
-CommonGarden.prototype._write = function (chunk, encoding, callback) {
+GROWJS.prototype._write = function (chunk, encoding, callback) {
   var self = this;
 
   self.sendData(chunk, callback);
@@ -129,8 +133,78 @@ CommonGarden.prototype._write = function (chunk, encoding, callback) {
 
 // We are pushing data to a stream as commands are arriving and are leaving
 // to the stream to buffer them. So we simply ignore requests for more data.
-CommonGarden.prototype._read = function (size) {
+GROWJS.prototype._read = function (size) {
   var self = this;
 };
 
-module.exports = CommonGarden;
+
+// Takes a model and updates the state property.
+GROWJS.prototype.updateState = function (model, state, callback) {
+  model.properties[0].state = state;
+  // writes to grow file.
+};
+
+  // May
+}
+
+// Takes the model and updates the crons property
+GROWJS.prototype.updateCrons = function (model, newCrons) {
+  model.properties[0].crons;
+};
+
+
+// TODO should run the code currently at the begining of the config file.
+GROWJS.prototype.onConnect = function (error, data) {
+  if (error) return console.log("Error", error);
+
+  /* Now check to see if we have a stored UUID.
+   * If no UUID is specified, store a new UUID. */
+  if (_.isUndefined(config.uuid) || _.isUndefined(config.token)) {
+      config.uuid = data.uuid;
+      config.token = data.token;
+      fs.writeFile('./config.json', JSON.stringify(config, null, 4), function (error) {
+          if (error) return console.log("Error", error);
+
+          console.log("New configration was saved with a uuid of: " + data.uuid);
+      });  
+  }
+
+  // Make a new readable stream
+  var readableStream = new Readable({objectMode: true});
+
+  // Make a new writable stream
+  var writableStream = new Writable({objectMode: true});
+
+  // We are pushing data when sensor measures it so we do not do anything
+  // when we get a request for more data. We just ignore it.
+  readableStream._read = function () {};
+
+  // We catch any errors
+  readableStream.on('error', function (error) {
+    console.log("Error", error.message);
+  });
+}
+
+// This could be very useful in grow.js, as scheduling tasks will be important.
+// Stop crons
+GROWJS.prototype.stopCrons = function (crons) {
+  for (var key in crons) {
+     if (crons.hasOwnProperty(key)) {
+        var obj = crons[key];
+        obj.stop();
+     }
+  }
+}
+
+// Start crons
+GROWJS.prototype.startCrons = function (crons) {
+  for (var key in crons) {
+     if (crons.hasOwnProperty(key)) {
+        var obj = crons[key];
+        obj.start();
+     }
+  }
+  console.log("Started crons");
+}
+
+module.exports = GROWJS;
